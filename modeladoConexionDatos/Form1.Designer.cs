345bd106f543224e1b431ad86c01cbedd26f67ef
cmendoza-dev <carlos.mendoza.s@edu.pe>
2024-04-04 23:04:15 -0500

    Agregar archivos de proyecto.

---
+++ modeladoConexionDatos/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace modeladoConexionDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBD = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chckAuth = new System.Windows.Forms.CheckBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPass);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtUsuario);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtBD);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtServidor);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(29, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 150);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(32, 50);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Base de Datos";
+            // 
+            // txtBD
+            // 
+            this.txtBD.Location = new System.Drawing.Point(32, 102);
+            this.txtBD.Name = "txtBD";
+            this.txtBD.Size = new System.Drawing.Size(100, 20);
+            this.txtBD.TabIndex = 3;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(200, 102);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(200, 50);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Usuario";
+            // 
+            // chckAuth
+            // 
+            this.chckAuth.AutoSize = true;
+            this.chckAuth.Location = new System.Drawing.Point(61, 200);
+            this.chckAuth.Name = "chckAuth";
+            this.chckAuth.Size = new System.Drawing.Size(138, 17);
+            this.chckAuth.TabIndex = 8;
+            this.chckAuth.Text = "Autenticación Windows";
+            this.chckAuth.UseVisualStyleBackColor = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnConectar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnConectar.Location = new System.Drawing.Point(301, 238);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 9;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 292);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.chckAuth);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio de Sesión";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chckAuth;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ modeladoConexionDatos/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modeladoConexionDatos
+{
+    public partial class Form1 : Form
+    {
+
+
+        void conectarAuthWindows()
+        { 
+            string servidor = txtServidor.Text;
+            string dbname = txtBD.Text;
+
+            // Concatenar en la cadena de conexión
+            string cadenaConexion = $"Data Source={servidor};Initial Catalog={dbname};Integrated Security=True;Encrypt=False";
+
+            // Crear la conexión
+            SqlConnection cnx = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                cnx.Open();
+                MessageBox.Show("Autenticación Windows");
+                
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void conectarAuthSQLServer()
+        {
+            string user = txtUsuario.Text;
+            string dbPass = txtPass.Text;
+
+            // Concatenar en la cadena de conexión
+            string cadenaConexion = $"Data Source =.; Initial Catalog = tempdb; User ID = {user}; Password = {dbPass}; Encrypt = False";
+            
+            // Crear la conexión
+            SqlConnection cnx = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                cnx.Open();
+                MessageBox.Show("Autenticación SQLServer");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (chckAuth.Checked)
+            {
+                conectarAuthWindows();
+            } else
+            {
+                conectarAuthSQLServer();
+            }
+
+            
+        }
+    }
+}
